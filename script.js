703d4c8b086e81fde06487a45732b6e93979cee9
Zach Corpuz <zachcorpuz@yahoo.com>
2020-04-30 16:28:36 -0700

    Refactored code so that the colorblock was a universal function

--- script.js
+++ script.js
@@ -4,13 +4,49 @@ $(document).ready(function() {
     var currentDate = moment().format("MMMM Do YYYY, h:mm a");      
     $("#currentDay").append(currentDate);
 
+    // Compare time slot to current time in order to change the color of the block
+    function colorBlock() {
+
+        // Set current hour in military time to negate the use of AM/PM
+        var currentHour = moment().format('H');
+
+        // Following Function will seperate values of each block
+        $(".eventRow").each(function() {
+            var eventBlock = parseInt($(this).attr("value"));
+
+            // Compare each timeblock to current hour
+            if (eventBlock < currentHour) {
+                $(this).addClass("past");
+            }else if (eventBlock === currentHour) {
+                $(this).addClass("present");
+            }else if (eventBlock > currentHour) {
+                $(this).addClass("future");
+            }
+        })
+    }
+
+    // function setEvents() {
+    //     $(this).empty();
+    //     $(this).append(localStorage.getItem("Event")); 
+    // }
+
+
+    // localStorage.setItem("9am Event", ($("#eventRow1").html()));
+    // var savedEvents = localStorage.getItem("9am Event");
+    // $("eventRow1").html(savedEvents);
+    // console.log(saveEvents);
+    // $(".eventRow").append(localStorage.getItem("9am Event"));
+    // localStorage.setItem("Event", $(".eventRow").html());
+    // eventAdd.replace(localStorage.getItem("Event"));
+
+    colorBlock();
+
     // Below are the click events for each row so that the user can input what events they want for each hour
     $("#eventRow1").on("click", function() {
         var eventAdd = prompt("What event would you like to add?");
         $("#eventRow1").text(eventAdd);
-        localStorage.setItem("9am Event", ($("#eventRow1").html()));
-        localStorage.getItem("9am Event")
     });
+    
 
     $("#eventRow2").on("click", function() {
         var eventAdd = prompt("What event would you like to add?");
@@ -51,20 +87,4 @@ $(document).ready(function() {
         var eventAdd = prompt("What event would you like to add?");
         $("#eventRow9").text(eventAdd);
     });
-
-    colorBlock();
-
-    // Compare time slot to current time in order to change the color of the block
-    function colorBlock() {
-        var currentHour = moment().format('H');
-        if ($(".eventRow").attr("value") === currentHour) {
-            $(".eventRow").addClass("present");
-        } else if ($(".eventRow").attr("value") > currentHour) {
-            $(".eventRow").addClass("past");
-        } else if ($(".eventRow").attr("value") < currentHour) {
-            $(".eventRow").addClass("future");
-        }
-    }
 });
-
-
