7916c454a62cc690100cc7088c1d7952e6df4e08
Zach Corpuz <zachcorpuz@yahoo.com>
2020-04-29 19:15:03 -0700

    Aligned the tables. Made the prompt for the event block so that users can input their personal schedule

---
+++ script.js
@@ -0,0 +1,50 @@
+$(document).ready(function() {
+
+    $("#eventRow1").on("click", function(event) {
+        var eventAdd = prompt("What event would you like to add?");
+        $("#eventRow1").text(eventAdd);
+    });
+
+    $("#eventRow2").on("click", function() {
+        var eventAdd = prompt("What event would you like to add?");
+        $("#eventRow2").text(eventAdd);
+    });
+
+    $("#eventRow3").on("click", function() {
+        var eventAdd = prompt("What event would you like to add?");
+        $("#eventRow3").text(eventAdd);
+    });
+
+    $("#eventRow4").on("click", function() {
+        var eventAdd = prompt("What event would you like to add?");
+        $("#eventRow4").text(eventAdd);
+    });
+
+    $("#eventRow5").on("click", function() {
+        var eventAdd = prompt("What event would you like to add?");
+        $("#eventRow5").text(eventAdd);
+    });
+
+    $("#eventRow6").on("click", function() {
+        var eventAdd = prompt("What event would you like to add?");
+        $("#eventRow6").text(eventAdd);
+    });
+
+    $("#eventRow7").on("click", function() {
+        var eventAdd = prompt("What event would you like to add?");
+        $("#eventRow7").text(eventAdd);
+    });
+
+    $("#eventRow8").on("click", function() {
+        var eventAdd = prompt("What event would you like to add?");
+        $("#eventRow8").text(eventAdd);
+    });
+
+    $("#eventRow9").on("click", function() {
+        var eventAdd = prompt("What event would you like to add?");
+        $("#eventRow9").text(eventAdd);
+    });
+
+});
+
+
