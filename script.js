b7c29f66bad4c3231c50c8efcfc78f26fba350f4
Zach Corpuz <zachcorpuz@yahoo.com>
2020-09-24 21:05:23 -0700

    Refactored code to ES6

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 $(document).ready(function() {
 
     // This sets the current date at the top of the page
-    var currentDate = moment().format("MMMM Do YYYY, h:mm a");      
+    let currentDate = moment().format("MMMM Do YYYY, h:mm a");      
     $("#currentDay").append(currentDate);
 
     // Compare time slot to current time in order to change the color of the block
-    function colorBlock() {
+    const colorBlock = () => {
 
         // Set current hour in military time to negate the use of AM/PM
-        var currentHour = moment().format('H');
+        let currentHour = moment().format('H');
 
         // Following Function will seperate values of each block
-        $(".eventRow").each(function() {
+        $(".eventRow").each(() => {
             var eventBlock = parseInt($(this).attr("value"));
 
             // Compare each timeblock to current hour
@@ -40,65 +40,65 @@ $(document).ready(function() {
     $("#eventRow9").append(localStorage.getItem("5pm Event"));
 
     // Below are the click events for each row so that the user can input what events they want for each hour
-    $("#eventRow1").on("click", function() {
-        var eventAdd = prompt("What event would you like to add?");
+    $("#eventRow1").on("click", () => {
+        const eventAdd = prompt("What event would you like to add?");
         $("#eventRow1").text(eventAdd);
         // Save to Local Storage
         localStorage.setItem("9am Event", ($("#eventRow1").html()));
     });
     
 
-    $("#eventRow2").on("click", function() {
-        var eventAdd = prompt("What event would you like to add?");
+    $("#eventRow2").on("click", () => {
+        const eventAdd = prompt("What event would you like to add?");
         $("#eventRow2").text(eventAdd);
         // Save to Local Storage
         localStorage.setItem("10am Event", ($("#eventRow2").html()));
     });
 
-    $("#eventRow3").on("click", function() {
-        var eventAdd = prompt("What event would you like to add?");
+    $("#eventRow3").on("click", () => {
+        const eventAdd = prompt("What event would you like to add?");
         $("#eventRow3").text(eventAdd);
         // Save to Local Storage
         localStorage.setItem("11am Event", ($("#eventRow3").html()));
     });
 
-    $("#eventRow4").on("click", function() {
-        var eventAdd = prompt("What event would you like to add?");
+    $("#eventRow4").on("click", () => {
+        const eventAdd = prompt("What event would you like to add?");
         $("#eventRow4").text(eventAdd);
         // Save to Local Storage
         localStorage.setItem("12pm Event", ($("#eventRow4").html()));
     });
 
-    $("#eventRow5").on("click", function() {
-        var eventAdd = prompt("What event would you like to add?");
+    $("#eventRow5").on("click", () => {
+        const eventAdd = prompt("What event would you like to add?");
         $("#eventRow5").text(eventAdd);
         // Save to Local Storage
         localStorage.setItem("1pm Event", ($("#eventRow5").html()));
     });
 
-    $("#eventRow6").on("click", function() {
-        var eventAdd = prompt("What event would you like to add?");
+    $("#eventRow6").on("click", () => {
+        const eventAdd = prompt("What event would you like to add?");
         $("#eventRow6").text(eventAdd);
         // Save to Local Storage
         localStorage.setItem("2pm Event", ($("#eventRow6").html()));
     });
 
-    $("#eventRow7").on("click", function() {
-        var eventAdd = prompt("What event would you like to add?");
+    $("#eventRow7").on("click", () => {
+        const eventAdd = prompt("What event would you like to add?");
         $("#eventRow7").text(eventAdd);
         // Save to Local Storage
         localStorage.setItem("3pm Event", ($("#eventRow7").html()));
     });
 
-    $("#eventRow8").on("click", function() {
-        var eventAdd = prompt("What event would you like to add?");
+    $("#eventRow8").on("click", () => {
+        const eventAdd = prompt("What event would you like to add?");
         $("#eventRow8").text(eventAdd);
         // Save to Local Storage
         localStorage.setItem("4pm Event", ($("#eventRow8").html()));
     });
 
-    $("#eventRow9").on("click", function() {
-        var eventAdd = prompt("What event would you like to add?");
+    $("#eventRow9").on("click", () => {
+        const eventAdd = prompt("What event would you like to add?");
         $("#eventRow9").text(eventAdd);
         // Save to Local Storage
         localStorage.setItem("5pm Event", ($("#eventRow9").html()));
